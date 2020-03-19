dd32ecd713e73545eec00950abc5a494567ea8f4
归零幻想 <yqmailsend@163.com>
2020-03-19 23:48:08 +0800

    初始化服务器项目

---
+++ src/main/java/edu/upc/mishuserver/MishuserverApplication.java
@@ -0,0 +1,13 @@
+package edu.upc.mishuserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MishuserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MishuserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/upc/mishuserver/MishuserverApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.upc.mishuserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MishuserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
