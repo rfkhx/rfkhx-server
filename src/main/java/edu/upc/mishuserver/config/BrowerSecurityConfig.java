a6e58029343e140dff0b7bd38d6494f046330caf
归零幻想 <yqmailsend@163.com>
2020-05-19 18:04:23 +0800

    权限部分小修改 “记住我”功能

--- src/main/java/edu/upc/mishuserver/config/BrowerSecurityConfig.java
+++ src/main/java/edu/upc/mishuserver/config/BrowerSecurityConfig.java
@@ -11,11 +11,14 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 public class BrowerSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private DataSource dataSource;
+    @Autowired
+    private UserDetailsService userDetailService;
 
     @Bean
     public PersistentTokenRepository persistentTokenRepository() {
@@ -27,9 +30,13 @@ public class BrowerSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                // .antMatchers("/admin", "/admin/**").authenticated()
-                .anyRequest().permitAll().and().formLogin();
+        http.formLogin()// 表单登陆方式
+                .and().rememberMe().tokenRepository(persistentTokenRepository()) // 配置 token 持久化仓库
+                .tokenValiditySeconds(3600 * 24 * 7) // remember 过期时间，单为秒
+                .userDetailsService(userDetailService) // 处理自动登录逻辑
+                .and().authorizeRequests().antMatchers("/admin", "/admin/**")// 后台页面需要登陆
+                .authenticated().anyRequest().permitAll()//其他页面允许访问
+                ;
     }
 
     @Bean
