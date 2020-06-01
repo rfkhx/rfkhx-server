31e461a441e58f0cc9e771cf2fe130a45f937b84
归零幻想 <yqmailsend@163.com>
2020-06-01 12:02:59 +0800

    整理代码

--- src/main/java/edu/upc/mishuserver/controller/UserController.java
+++ src/main/java/edu/upc/mishuserver/controller/UserController.java
@@ -1,27 +1,15 @@
 package edu.upc.mishuserver.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import edu.upc.mishuserver.dto.UserDto;
-import edu.upc.mishuserver.repositories.PrivilegeRepository;
-import edu.upc.mishuserver.repositories.RoleRepository;
-import edu.upc.mishuserver.repositories.UserRepository;
 import edu.upc.mishuserver.services.UserService;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private PrivilegeRepository privilegeRepository;
-    @Autowired
-    private RoleRepository roleRepository;
     @Autowired
     private UserService userService;
 
